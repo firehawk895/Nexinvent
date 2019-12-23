e33f82256b6f15cbe5d439f5eceae47fb7a6ff4d
Ankan Adhikari <feraasd@gmail.com>
2019-12-24 02:26:24 +0530

    increasing page size

--- nexinvent/settings/base.py
+++ nexinvent/settings/base.py
@@ -70,7 +70,7 @@ WSGI_APPLICATION = 'nexinvent.wsgi.application'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 100,
+    'PAGE_SIZE': 300,
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
     )
